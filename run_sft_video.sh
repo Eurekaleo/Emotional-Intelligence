1bed6649447ac18ef3269bd8b3eea1f5b9cf1e7d
Leo <141792823+Eurekaleo@users.noreply.github.com>
2025-09-27 16:01:03 +0800

    Add files via API

---
+++ run_sft_video.sh
@@ -0,0 +1,28 @@
+
+export DEBUG_MODE="true" 
+export LOG_PATH="./debug_log.txt"
+
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nproc_per_node="4" \
+    --nnodes="1" \
+    --node_rank="0" \
+    --master_addr="127.0.0.1" \
+    --master_port="12349" \
+    src/open_r1/sft_video.py \
+    --output_dir "./log/Qwen2.5-Omini-7B-Video-7B-cot-sft" \
+    --model_name_or_path "Qwen/Qwen2.5-Omini-7B-Instruct" \
+    --dataset_name "./xxxx.json" \
+    --deepspeed local_scripts/zero2.json \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 2 \
+    --learning_rate 1e-6 \
+    --logging_steps 1 \
+    --bf16 \
+    --report_to wandb \
+    --gradient_checkpointing true \
+    --attn_implementation flash_attention_2 \
+    --num_train_epochs 1 \
+    --run_name Qwen2.5-Omini-7B-Video-cot-sft \
+    --save_steps 1000 \
+    --max_grad_norm 5 \
+    --save_only_model true \
